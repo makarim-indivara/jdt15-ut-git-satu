5d970080b97de2f365894abf144d4121ae6a61c9
Nathanael Zorens <nathanaelzorens@gmail.com>
2025-03-07 22:21:26 +0700

    repository testing

--- user-service/src/test/java/com/user_service/app/repository/UserRepositoryTest.java
+++ user-service/src/test/java/com/user_service/app/repository/UserRepositoryTest.java
@@ -3,25 +3,36 @@ package com.user_service.app.repository;
 import com.user_service.app.data.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+@ExtendWith(MockitoExtension.class)
 public class UserRepositoryTest {
 
-    @Autowired
+    @Mock
     private UserRepository userRepository;
 
     @Test
     public void testInsert(){
-        User user = new User();
-        user.setId(1L);
-        user.setName("John");
-        user.setPassword("abc");
-        user.setPassword("john@email");
+        User user = new User("1","John","abc","John Doe");
 
         userRepository.insert(user);
 
         Assertions.assertNotNull(user);
 //        Assertions.assertNotEquals(,user.getId());
+        Mockito.verify(userRepository,Mockito.times(1)).insert(new User(user.getId(),"John","abc","John Doe"));
+    }
+
+    @Test
+    public void testFindById(){
+        User user = new User("1","John","abc","John Doe");
+
+        User getUser=userRepository.findbyId("1");
+
+        Assertions.assertEquals(user,getUser);
 
     }
 
